b6363386339c808d029b25effdc0ad409d744e57
Evandro Gandelini <evandrogandelini@atec.com.br>
2017-12-05 20:18:48 -0200

    first commit

---
+++ hello.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class program
+{
+public static void main(string[] args)
+{
+console.writeline("Hello World!");
+if args( != null)
+{
+console.writeline("Args:");
+foreach (var arg in args)
+{
+console.writeline(arg);
+
+}
+
+}
+
+}
+}
